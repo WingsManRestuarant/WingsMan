523ba5a827c2238a8a8bf277cbf86ecf0ddf0050
Zesshi <kopmat65@gmail.com>
2023-03-04 18:56:32 +0700

    Second commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 // set the serving static files in the express in a directory named public
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static("public"));
 
 // use bodyParser for URL encoding
 app.use(bodyParser.urlencoded({ extended: true }));
