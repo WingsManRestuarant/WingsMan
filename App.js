9483ba36282ee9002804f07fd7052941b5bdb5b2
Yotsawat Lekwongthanasin <kopmat65@gmai.com>
2023-03-13 20:14:51 +0700

    add cart and progress page

--- App.js
+++ App.js
@@ -31,6 +31,13 @@ app.get("/category/drink", function (req, res) {
     res.render("drink",)
 });
 
+app.get("/cart", function(req, res){
+    res.render("cart")
+})
+app.get("/progress", function(req, res){
+    res.render("progress")
+})
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
