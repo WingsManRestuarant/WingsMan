5995e63add78e76ac153e2c805f60c8df9bbcb15
patrickhum <jirapat_na@cmu.ac.th>
2023-03-21 13:50:20 +0700

    add login page

--- App.js
+++ App.js
@@ -37,7 +37,9 @@ app.get("/cart", function(req, res){
 app.get("/progress", function(req, res){
     res.render("progress")
 })
-
+app.get("/login", function(req, res){
+    res.render("login")
+})
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
