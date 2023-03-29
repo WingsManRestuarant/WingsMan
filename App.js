96c02ffa5ac3983d7ed9663752006949f529cba7
patrickhum <jirapat_na@cmu.ac.th>
2023-03-29 09:51:43 +0700

    add register page

--- App.js
+++ App.js
@@ -31,15 +31,20 @@ app.get("/category/drink", function (req, res) {
     res.render("drink",)
 });
 
-app.get("/cart", function(req, res){
+app.get("/cart", function (req, res) {
     res.render("cart")
 })
-app.get("/progress", function(req, res){
+app.get("/progress", function (req, res) {
     res.render("progress")
 })
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login")
 })
+
+app.get("/register", function (req, res) {
+    res.render("register")
+})
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
